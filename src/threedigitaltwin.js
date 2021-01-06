dc44350f8096dcfa225eb647653cb221a298214b
Rui Jorge Ramos <rui.j.ramos@inesctec.pt>
2021-01-06 16:28:29 +0000

    .

--- src/threedigitaltwin.js
+++ src/threedigitaltwin.js
@@ -6,6 +6,8 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { KMZLoader } from 'three/examples/jsm/loaders/KMZLoader.js';
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
 import { OBJLoader2 } from 'three/examples/jsm/loaders/OBJLoader2.js';
+import { centroid } from '@turf/centroid'
+import polygon from "@turf/helpers";
 
 CameraControls.install({ THREE: THREE });
 
@@ -13,7 +15,7 @@ CameraControls.install({ THREE: THREE });
 // const REFRESH_TIMEOUT = 500; //ms
 const near = 5;
 const far = 3500;
-//var offset = 0;
+var offset = 0;
 
 class ThreeDigitalTwin {
 
@@ -40,7 +42,7 @@ class ThreeDigitalTwin {
         this._clock = new THREE.Clock();
         this._canvas = canvas;
         this._camera = null;
-        this._scene = null;
+        this.scene = null;
         this._renderer = null;
         this._controls = null;
         this._meshes = [];
@@ -56,7 +58,7 @@ class ThreeDigitalTwin {
     initScene() {
 
         /// Init Scene
-        this._scene = new THREE.Scene();
+        this.scene = new THREE.Scene();
 
         /// Init Camera
         this._camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, near, far);
@@ -79,25 +81,25 @@ class ThreeDigitalTwin {
         /// Init Lights
         let light = new THREE.PointLight(0xffffff);
         light.position.set(0, 150, 100);
-        this._scene.add(light);
+        this.scene.add(light);
 
         //Ambient light
         this._ambientLight = new THREE.AmbientLight(0xffffff, 0.65);
-        this._scene.add(this._ambientLight);
+        this.scene.add(this._ambientLight);
 
         //Hemisphere Light
         var hlight = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.4);
-        this._scene.add(hlight);
+        this.scene.add(hlight);
 
         // Fog
         if(this.fog) {
-            this._scene.fog = new THREE.Fog(0xFFFFFF, far/3, far/2);
+            this.scene.fog = new THREE.Fog(0xFFFFFF, far/3, far/2);
         }
 
         /// Helpers (@remove)
         // const size = 100;
         // const axesHelper = new THREE.AxesHelper(size);
-        // this._scene.add(axesHelper);
+        // this.scene.add(axesHelper);
 
         // Create a map tiles provider object
         var provider = new GeoThree.MapBoxProvider(
@@ -109,7 +111,7 @@ class ThreeDigitalTwin {
         // Create the map view and add it to your THREE scene
         var map = new GeoThree.MapView(GeoThree.MapView.PLANAR, provider);
         map.position.set(-this.centerInMeters[0], 0, this.centerInMeters[1]);
-        this._scene.add(map);
+        this.scene.add(map);
 
         this._animate();
 
@@ -147,7 +149,7 @@ class ThreeDigitalTwin {
 
         requestAnimationFrame(this._animate.bind(this));
 
-        this._renderer.render(this._scene, this._camera);
+        this._renderer.render(this.scene, this._camera);
         
     }
 
@@ -161,7 +163,7 @@ class ThreeDigitalTwin {
     }
 
     removeSceneLayers() {
-        this._clearThree(this._scene);
+        this._clearThree(this.scene);
     }
 
     loadGeoJSON(url) {
@@ -302,7 +304,7 @@ class ThreeDigitalTwin {
                         else lod.addLevel(cube, 1500);
                         lod.position.copy(targetPosition);
 
-                        this._scene.add(lod);
+                        this.scene.add(lod);
                     },
 
                     undefined,
@@ -352,7 +354,7 @@ class ThreeDigitalTwin {
                 else lod.addLevel(cube, 1500);
                 lod.position.copy(targetPosition);
 
-                this._scene.add(lod);
+                this.scene.add(lod);
             },
 
             // onProgress callback
@@ -367,12 +369,285 @@ class ThreeDigitalTwin {
 
     }
 
-    showGridHelper() {
-        const gridHelper = new THREE.GridHelper(3000, 10)
-        this._scene.add(gridHelper);
+    async loadLayer(layerCode, geojson, properties, type) {
+
+        if (geojson == null || geojson.features == null) return;
+
+        var geo = utils.convertGeoJsonToWorldUnits(geojson);
+        var shape = null;
+        var modelGLTF = null;
+        var values;
+        var feature;
+
+        switch (type) {
+            case "EXTRUDE":
+
+                for (feature of geo.features) {
+                    feature.layerCode = layerCode;
+                    feature.properties = Object.assign({}, properties, feature.properties);
+
+                    shape = this.createShape(feature);
+
+                    if (shape) {
+                        this.scene.add(shape);
+
+                        if (layerCode) {
+                            values = [];
+                            if (this.layers.get(layerCode)) {
+                                values = this.layers.get(layerCode);
+                            }
+                            values.push(shape);
+                            this.layers.set(layerCode, values);
+                        }
+
+                        shape.geometry.dispose();
+                    }
+
+                    offset += 1;
+                }
+
+                this.dispatch('layerloaded', layerCode);
+
+                break;
+            case "GLTF":
+                for (feature of geo.features) {
+                    feature.layerCode = layerCode;
+                    feature.properties = Object.assign({}, properties, feature.properties);
+
+                    modelGLTF = await this.createModelGLTF(feature);
+
+                    if (modelGLTF) {
+                        this.scene.add(modelGLTF);
+
+                        if (layerCode) {
+                            values = [];
+                            if (this.layers.get(layerCode)) {
+                                values = this.layers.get(layerCode);
+                            }
+                            values.push(modelGLTF);
+                            this.layers.set(layerCode, values);
+                        }
+                    }
+                }
+
+                this.dispatch('layerloaded', layerCode);
+
+                break;
+            default:
+                console.log('default');
+        }
     }
 
-    _dispatch(eventName, data) {
+    //1- Buildings 2- Warehouses 3- Roads 4- Gardens 5- Parking slots 
+    createShape(feature) {
+
+        var shapearray = this.calcVertices(feature);
+        var textureTop;
+        var textureSide;
+
+        if (feature.properties.material.textureTop) {
+            textureTop = new THREE.TextureLoader().load(feature.properties.material.textureTop) || null;
+            textureTop.wrapS = THREE.RepeatWrapping;
+            textureTop.wrapT = THREE.RepeatWrapping;
+            textureTop.flipY = false;
+        }
+
+        if (feature.properties.material.textureSide) {
+            textureSide = new THREE.TextureLoader().load(feature.properties.material.textureSide) || null;
+            textureSide.wrapS = THREE.RepeatWrapping;
+            textureSide.wrapT = THREE.RepeatWrapping;
+            textureSide.flipY = false;
+        }
+
+        var material = [new THREE.MeshPhongMaterial({
+            color: new THREE.Color(feature.properties.material.colorTop) || null,
+            opacity: feature.properties.material.opacityTop,
+            transparent: true,
+            map: textureTop || null,
+            polygonOffset: feature.properties.material.polygonOffset || false, // fix overlapping problems
+            polygonOffsetFactor: feature.properties.material.polygonOffsetFactor || -1, // fix overlapping problems
+            polygonOffsetUnits: feature.properties.material.polygonOffsetUnits - offset || -1 // fix overlapping problems
+        }), new THREE.MeshPhongMaterial({
+            color: new THREE.Color(feature.properties.material.colorSide) || null,
+            opacity: feature.properties.material.opacitySide,
+            transparent: true,
+            map: textureSide || null,
+            polygonOffset: feature.properties.material.polygonOffset || false, // fix overlapping problems
+            polygonOffsetFactor: feature.properties.material.polygonOffsetFactor || -1, // fix overlapping problems
+            polygonOffsetUnits: feature.properties.material.polygonOffsetUnits - offset || -1// fix overlapping problems
+        })]
+
+        var extrudeSettings = {
+            depth: feature.properties.depth,
+            bevelEnabled: false,
+            bevelSegments: 1,
+            steps: 5,
+            bevelSize: 0,
+            bevelThickness: 1
+        };
+
+        var shape3D = new THREE.ExtrudeBufferGeometry(shapearray, extrudeSettings);
+        shape3D.translate(-this.centerInMeters[0], -this.centerInMeters[1], feature.properties.altitude);
+        var mesh = new THREE.Mesh(shape3D, material);
+
+
+        if (textureTop) {
+            this.adjustTextureTopRepeat(mesh, feature.properties.material.textureSizeTop);
+        }
+
+        if (textureSide) {
+            this.adjustTextureSideRepeat(mesh, feature.properties.material.textureSizeSide);
+        }
+
+        mesh.matrixAutoUpdate = false;
+        mesh.receiveShadow = false;
+        mesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), - Math.PI / 2);
+        mesh.updateMatrix();
+
+        shape3D.dispose();
+
+        return mesh;
+    }
+
+    async createModelGLTF(feature) {
+        var coordX;
+        var coordY;
+        if (feature.geometry.type != "Point") {
+            var centroid_obj = centroid(polygon(feature.geometry.coordinates));
+            coordX = centroid_obj.geometry.coordinates[0];
+            coordY = centroid_obj.geometry.coordinates[1];
+        } else {
+            coordX = feature.geometry.coordinates[0];
+            coordY = feature.geometry.coordinates[1];
+        }
+
+        var mesh;
+        await this.loadGLTF(feature.properties.model).then((object) => {
+
+            object.position.set(coordX - this.centerInMeters[0], feature.properties.altitude, -(coordY - this.centerInMeters[1]));
+
+            object.matrixAutoUpdate = false;
+            object.receiveShadow = false;
+            object.updateMatrix();
+
+            mesh = object;
+        });
+
+        return mesh;
+    }
+
+    moveObjectToCoordinates(mesh, lon, lat) {
+        var coordinates = this.convertCoordinatesToUnits(lon, lat);
+        var targetPosition = new THREE.Vector3(coordinates[0] - this.centerInMeters[0], mesh.position.y, -(coordinates[1] - this.centerInMeters[1]));
+        mesh.position.copy(targetPosition);
+    }
+
+    rotateObject(object, axis, angle) {
+        let vector;
+        if (axis == "x")        vector = new THREE.Vector3(1,0,0);
+        else if (axis == "y")   vector = new THREE.Vector3(0,1,0);
+        else if (axis == "z")   vector = new THREE.Vector3(0,0,1);
+        object.rotateOnAxis(vector, angle)
+    }
+
+    setAltitude(object, altitude) {
+        object.position.y = altitude;
+    }
+
+    adjustTextureTopRepeat(mesh, textureSize) {
+
+        mesh.geometry.computeBoundingBox();
+        let max = mesh.geometry.boundingBox.max;
+        let min = mesh.geometry.boundingBox.min;
+        let height = max.y - min.y;
+        let width = max.x - min.x;
+
+        let repeatValX = width / textureSize;
+        let repeatValY = height / textureSize;
+        if (repeatValX < 0.1) {
+            repeatValX *= 10;
+        } else if (repeatValX > 0.45) {
+            repeatValX /= 2;
+        }
+        if (repeatValY < 0.1) {
+            repeatValY *= 10;
+        }
+
+        mesh.material[0].map.repeat.set(repeatValX, repeatValY);
+    }
+
+    adjustTextureSideRepeat(mesh, textureSize) {
+
+        mesh.geometry.computeBoundingBox();
+        let max = mesh.geometry.boundingBox.max;
+        let min = mesh.geometry.boundingBox.min;
+
+        let height = max.z - min.z;
+        let width = max.x - min.x;
+
+        let repeatValX = width / textureSize;
+        let repeatValY = height / textureSize;
+        if (repeatValX < 0.1) {
+            repeatValX *= 10;
+        } else if (repeatValX > 0.45) {
+            repeatValX /= 2;
+        }
+        if (repeatValY < 0.1) {
+            repeatValY *= 10;
+        }
+
+        mesh.material[1].map.repeat.set(repeatValX, repeatValY);
+    }
+
+    loadGLTF(objPath) {
+        return new Promise((resolve) => {
+            const loader = new GLTFLoader();
+            loader.load(
+                objPath,
+                (gltf) => {
+                    gltf.scene.children.forEach((element) => {
+                        if (element.material) {
+                            element.material.metalness = 0;
+                        }
+                    });
+                    resolve(gltf.scene);
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+        });
+    }
+
+    removeLayer(layerCode) {
+        if (layerCode) {
+            var meshes = this.layers.get(layerCode);
+            if (meshes && meshes.length > 0) {
+                meshes.forEach(mesh => {
+                    if (mesh && mesh.geometry) {
+                        mesh.geometry.dispose();
+                        this.scene.remove(mesh);
+                    } else if (mesh) {
+                        this.scene.remove(mesh);
+                    }
+                });
+                this.layers.delete(layerCode);
+            }
+
+        }
+    }
+
+    clear() {
+        var context = this.canvas.getContext("2d");
+        context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    showGridHelper() {
+        const gridHelper = new THREE.GridHelper(3000, 10)
+        this.scene.add(gridHelper);
+    }
+
+    dispatch(eventName, data) {
         const event = this.events[eventName];
         if (event) {
             event.fire(data);
